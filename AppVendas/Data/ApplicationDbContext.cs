870e9412b2e7ab63c2a2e74ef43cf8513ebd2e62
Bryan Fusco <bryanratin8@gmail.com>
2024-09-11 15:14:59 -0300

    pasta data / mudando arquivo para tabela

--- AppVendas/Data/ApplicationDbContext.cs
+++ AppVendas/Data/ApplicationDbContext.cs
@@ -1,7 +1,37 @@
-﻿namespace AppVendas.Data
+﻿using AppVendas.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace AppVendas.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<ItemDaVenda> ItensDaVenda { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Cliente>().ToTable("Clientes");
+            modelBuilder.Entity<Categoria>().ToTable("Categoria");
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+            modelBuilder.Entity<Venda>().ToTable("Venda");
+            modelBuilder.Entity<ItemDaVenda>().ToTable("ItensDaVenda");
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
