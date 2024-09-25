fca27a88372f5af0c645b4f13c6757da972e72ee
Bryan Fusco <bryanratin8@gmail.com>
2024-09-25 11:53:59 -0300

    Finalizada a view index de produtos

--- AppVendas/Models/Produto.cs
+++ AppVendas/Models/Produto.cs
@@ -1,15 +1,30 @@
-﻿namespace AppVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendas.Models
 {
     public class Produto
     {
         public Guid ProdutoId { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo não pode ser vazio!")]
+        [MaxLength(100, ErrorMessage = "O campo deve ter, no maximo, 100 caracteres!")]
+        [MinLength(3, ErrorMessage = "O campo deve ter, no minimo, 3 caracteres!")]
         public string ProdutoNome { get; set; }
+
+        [Required(ErrorMessage = "O campo não pode ser vazio!")]
+        [Range(0, double.MaxValue, ErrorMessage = "O valor do produto deve ser um número positivo")]
         public double Valor { get; set; }
+
+        [Display(Name = "Estoque Atual")]
+        [Required(ErrorMessage = "O campo não pode ser vazio!")]
+        [Range(0, double.MaxValue, ErrorMessage = "A quantidade em Estoque deve ser positivo")]
         public double QtadeEstoque { get; set; }
-        public bool CadastroAtivo { get; set; }
+        [Display(Name = "Ativo?")]
+        public bool CadastroAtivo { get; set; } = true;
 
         //chave estrangeira como fazer referencia outra tabela
-
+        [Required(ErrorMessage = "Por favor selecione uma Categoria!")]
         public Guid CategoriaId { get; set; }
         public Categoria? Categoria { get; set; }
 
